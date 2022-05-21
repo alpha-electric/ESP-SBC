a22ac0b23a8fa0c9320e8bf6d127422f5bc15eca
Magmanat <princemjp@gmail.com>
2022-05-21 17:51:47 +0800

    changed to batch update

--- simplesub.py
+++ simplesub.py
@@ -15,26 +15,16 @@ global Batt2timer
 Batt2timer = time.time()
 Batt2timeout = 15
 
-global Batt1timedout
-global Batt2Timedout
-Batt1timedout = False
-Batt2timedout = False
-
 def send_blynk(batt, received_data, refresh = True):
-    global Batt1timedout
-    global Batt2timedout
     global Batt1timer
     global Batt2timer
+    print(requests.get("https://blynk.cloud/external/api/update?token=" + BLYNK_AUTH_TOKEN + "&v14=ONLINE"))
     if batt == 1:
-        if refresh:
-            Batt1timedout = False
-            Batt1timer = time.time()
+        Batt1timer = time.time()
         print(requests.get("https://blynk.cloud/external/api/batch/update?token=" + BLYNK_AUTH_TOKEN + "&v6=" + received_data[0] + "&v7=" + received_data[1] + "&v8=" + received_data[2] + "&v9=" + received_data[3] + "&v10=" + received_data[4] + "&v11=" + received_data[5] + "&v12=" + received_data[6]))
         print("updated batt 1")
     elif batt == 2:
-        if refresh:
-            Batt2timedout = False
-            Batt2timer = time.time()
+        Batt2timer = time.time()
         print(requests.get("https://blynk.cloud/external/api/batch/update?token=" + BLYNK_AUTH_TOKEN + "&v13=" + received_data[0] + "&v0=" + received_data[1] + "&v1=" + received_data[2] + "&v2=" + received_data[3] + "&v3=" + received_data[4] + "&v4=" + received_data[5] + "&v5=" + received_data[6]))
         print("updated batt 2")
 
@@ -99,13 +89,9 @@ client.subscribe(topic_name)
 client.on_message = on_message
 
 while True:
-    if Batt1timedout == False and time.time() - Batt1timer >= Batt1timeout:
-        Batt1timedout = True
-        send_blynk(1,["Disconnected","0","0","0","0","0","0","0","0","0","0","0","0"],False)
+    if time.time() - Batt1timer >= Batt1timeout:
         BatteryList[0] = ""
-    if Batt2timedout == False and time.time() - Batt2timer >= Batt2timeout:
-        Batt2timedout = True
-        send_blynk(2,["Disconnected","0","0","0","0","0","0","0","0","0","0","0","0"],False)
+    if time.time() - Batt2timer >= Batt2timeout:
         BatteryList[1] = ""
     
     
