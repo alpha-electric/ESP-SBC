2c9864f7b633974b5d7a2cd0c48d5f30856e8857
Magmanat <princemjp@gmail.com>
2022-05-21 19:52:56 +0800

    added try except for if no internet connection

--- send_csv.py
+++ send_csv.py
@@ -6,21 +6,24 @@ from os import listdir
 Dir = "/home/pi/DataFiles"
 
 FileList = listdir(Dir)
-url = "https://httpbin.org/post"
+url = "http://api.alphaelectrics.co/csv"
 
 for i in FileList:
     tries = 0
     while tries < 5:
-        file = {'file': open(Dir + "/" + i, "rb")}
-        r = requests.post(url, files=file)
-        print(r.status_code)
-        print(r.text)
-        if r.status_code == 200:
-            os.remove(Dir + "/" + i)
-            break
-        else:
-            tries += 1
-            print("failed to send files")
+        try:
+            file = {'file': open(Dir + "/" + i, "rb")}
+            r = requests.post(url, files=file)
+            print(r.status_code)
+            print(r.text)
+            if r.status_code == 200:
+                os.remove(Dir + "/" + i)
+                break
+            else:
+                tries += 1
+                print("failed to send files")
+        except:
+            print("no internet connection")
         
 
 
