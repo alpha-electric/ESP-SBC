c525fc4d422b288af468381da70bc4f59899cc3e
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2022-06-04 14:00:06 +0800

    changed online http get update into the batch update
    changed online http get update into the batch update

--- simplesub.py
+++ simplesub.py
@@ -26,16 +26,15 @@ def send_blynk(batt, received_data, refresh = True):
     global Batt1timedout
     global Batt2timedout
     try:
-        print(requests.get("https://sgp1.blynk.cloud/external/api/update?token=" + BLYNK_AUTH_TOKEN + "&v14=1").text)
         if batt == 1:
             Batt1timer = time.time()
             Batt1timedout = False
-            print(requests.get("https://sgp1.blynk.cloud/external/api/batch/update?token=" + BLYNK_AUTH_TOKEN + "&v6=" + received_data[0] + "&v7=" + received_data[1] + "&v8=" + received_data[2] + "&v9=" + received_data[3] + "&v10=" + received_data[4] + "&v11=" + received_data[5] + "&v12=" + received_data[6]).text)
+            print(requests.get("https://sgp1.blynk.cloud/external/api/batch/update?token=" + BLYNK_AUTH_TOKEN + "&v6=" + received_data[0] + "&v7=" + received_data[1] + "&v8=" + received_data[2] + "&v9=" + received_data[3] + "&v10=" + received_data[4] + "&v11=" + received_data[5] + "&v12=" + received_data[6] + "&v14=1").text)
             print("updated batt 1")
         elif batt == 2:
             Batt2timer = time.time()
             Batt2timedout = False
-            print(requests.get("https://sgp1.blynk.cloud/external/api/batch/update?token=" + BLYNK_AUTH_TOKEN + "&v13=" + received_data[0] + "&v0=" + received_data[1] + "&v1=" + received_data[2] + "&v2=" + received_data[3] + "&v3=" + received_data[4] + "&v4=" + received_data[5] + "&v5=" + received_data[6]).text)
+            print(requests.get("https://sgp1.blynk.cloud/external/api/batch/update?token=" + BLYNK_AUTH_TOKEN + "&v13=" + received_data[0] + "&v0=" + received_data[1] + "&v1=" + received_data[2] + "&v2=" + received_data[3] + "&v3=" + received_data[4] + "&v4=" + received_data[5] + "&v5=" + received_data[6] + "&v14=1").text)
             print("updated batt 2")
     except:
         print("No internet connection")
